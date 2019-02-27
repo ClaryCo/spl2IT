da9ee16f8269af1c5b1c7c3b82e0be840625ea6b
Cojocaru Clarisa <clarisawolf24@gmail.com>
2019-02-27 14:36:49 +0100

    created 01_my-age.py

---
+++ 01_my-age.py
@@ -0,0 +1,13 @@
+#01_my-age.py
+#1.Nach dem Namen fragen un diesen in einer Varibale speichern
+#2.Nach dem Alter fragen und ebenso peichern 
+#3. Ausgeben,wie alt man nächsten Jahr sein wird. 
+#BSP:
+#What is your name?"Clai"
+#How fdsfsdf "19"
+#Next year you will 20
+
+input("Wie heißt du")
+name = input("Clarisa")
+input("Wie alt bist du")
+alter = input
